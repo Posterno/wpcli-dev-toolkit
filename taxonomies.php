6121bd971d5df8541311b727b534e8b36123338a
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-16 20:21:00 +0100

    add new taxonomies

--- taxonomies.php
+++ taxonomies.php
@@ -38,5 +38,37 @@ add_action(
 		);
 		register_taxonomy( 'taxonomy2', array( 'listings' ), $args );
 
+		$labels = array(
+			'name'          => 'Taxonomy 3',
+			'singular_name' => 'Taxonomy 3',
+			'menu_name'     => 'Taxonomy 3',
+		);
+		$args   = array(
+			'labels'            => $labels,
+			'hierarchical'      => false,
+			'public'            => true,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'show_in_nav_menus' => true,
+			'show_tagcloud'     => true,
+		);
+		register_taxonomy( 'taxonomy3', array( 'listings' ), $args );
+
+		$labels = array(
+			'name'          => 'Taxonomy 4',
+			'singular_name' => 'Taxonomy 4',
+			'menu_name'     => 'Taxonomy 4',
+		);
+		$args   = array(
+			'labels'            => $labels,
+			'hierarchical'      => true,
+			'public'            => true,
+			'show_ui'           => true,
+			'show_admin_column' => true,
+			'show_in_nav_menus' => true,
+			'show_tagcloud'     => true,
+		);
+		register_taxonomy( 'taxonomy4', array( 'listings' ), $args );
+
 	}
 );
